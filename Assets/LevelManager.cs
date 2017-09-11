b93af95ef3c00994a49ca2c9ec337f8581a8ef91
445710992an <31056280+445710992an@users.noreply.github.com>
2017-09-11 12:50:06 +1000

    change

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour 
+{
+
+	private static bool firstStart = true;
+	public GameObject SplashScreen;
+
+	[Header("Splash Screen Display Config")]
+	public bool showSplashScreen = true;
+	public float splashScreenTime = 3f;
+
+	void start()
+	{
+		if(firstStart && showSplashScreen)
+		{
+			firstStart = false;
+			SplashScreen.SetActive(true);
+			StartCoroutine(CRHideSplashScreen(splashScreenTime));
+		}
+		else
+		{
+			SplashScreen.SetActive(false);
+		}
+	}
+
+	IEnumerator CRHideSplashScreen(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		SplashScreen.SetActive(false);
+
+		/*
+		if (!SoundManager.Instance.IsMusicOff() && SoundManager.Instance.MusicState != SoundManager.PlayingState.Playing)
+		{
+			SoundManager.Instance.PlayMusic(SoundManager.Instance.background);
+		}
+		*/
+	}
+}
