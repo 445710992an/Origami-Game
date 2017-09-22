916a879b05849093d7f629acb1b0327cbf9048d8
445710992an <31056280+445710992an@users.noreply.github.com>
2017-09-22 12:13:09 +1000

    change

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+		GameObject btnObj = GameObject.Find ("Button");
+		Button btn = btnObj.GetComponent<Button>();
+		btn.onClick.AddListener (delegate() {
+			this.GoNextScene(btnObj);
+		});
+	}
+
+	void Update () 
+	{
+
+	}
+
+	public void GoNextScene(GameObject Nscene)
+	{
+		int i = SceneManager.GetActiveScene().buildIndex;
+
+		SceneManager.LoadScene (i + 1);
+	}
+
+	// Update is called once per frame
+}
