6d0e074dbb69e376012b0692c860c0df3ab6d5c9
445710992an <31056280+445710992an@users.noreply.github.com>
2017-10-16 04:44:30 +1100

    cut and print

---
+++ Assets/Scripts/ControlPage.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPage : MonoBehaviour {
+
+	public GameObject LT;
+	public GameObject RT;
+	public GameObject LB;
+	public GameObject RB;
+
+	public GameObject RTtoLB;
+	public GameObject LBtoRT;
+
+	public GameObject RTtoRB;
+	public GameObject RTtoLT;
+	public GameObject LBtoLT;
+	public GameObject LBtoRB;
+
+	public GameObject LTtoRB;
+	public GameObject RBtoLT;
+
+	public GameObject ori;
+
+	bool boolLT = false;
+	bool boolRT = false;
+	bool boolRB = false;
+	bool boolLB = false;
+
+	void Start ()
+	{
+		ori.gameObject.SetActive (true);
+		RTtoRB.gameObject.SetActive (false);
+		RTtoLB.gameObject.SetActive (false);
+		RTtoLT.gameObject.SetActive (false);
+		LBtoLT.gameObject.SetActive (false);
+		LBtoRB.gameObject.SetActive (false);
+		LBtoRT.gameObject.SetActive (false);
+		LTtoRB.gameObject.SetActive (false);
+		RBtoLT.gameObject.SetActive (false);
+	}
+
+	void Update()
+	{
+		if((boolRB == true && boolRT == true))
+		{
+			RTtoRB.gameObject.SetActive (true);
+			RTtoLB.gameObject.SetActive (false);
+			RTtoLT.gameObject.SetActive (false);
+			LBtoLT.gameObject.SetActive (false);
+			LBtoRB.gameObject.SetActive (false);
+			LBtoRT.gameObject.SetActive (false);
+			LTtoRB.gameObject.SetActive (false);
+			RBtoLT.gameObject.SetActive (false);
+
+			ori.gameObject.SetActive (false);
+
+			boolRB = false;
+			boolRT = false;
+		}
+
+		if(boolRT == true && boolLB == true)
+		{
+			RTtoRB.gameObject.SetActive (false);
+			RTtoLB.gameObject.SetActive (true);
+			RTtoLT.gameObject.SetActive (false);
+			LBtoLT.gameObject.SetActive (false);
+			LBtoRB.gameObject.SetActive (false);
+			LBtoRT.gameObject.SetActive (false);
+			LTtoRB.gameObject.SetActive (false);
+			RBtoLT.gameObject.SetActive (false);
+
+			ori.gameObject.SetActive (false);
+
+			boolRT = false;
+			boolLB = false;
+		}
+
+		if(boolLT == true && boolRT == true)
+		{
+			RTtoRB.gameObject.SetActive (false);
+			RTtoLB.gameObject.SetActive (false);
+			RTtoLT.gameObject.SetActive (true);
+			LBtoLT.gameObject.SetActive (false);
+			LBtoRB.gameObject.SetActive (false);
+			LBtoRT.gameObject.SetActive (false);
+			LTtoRB.gameObject.SetActive (false);
+			RBtoLT.gameObject.SetActive (false);
+
+			ori.gameObject.SetActive (false);
+
+			boolLT = false;
+			boolRT = false;
+		}
+
+		if(boolLB == true && boolLT == true)
+		{
+			RTtoRB.gameObject.SetActive (false);
+			RTtoLB.gameObject.SetActive (false);
+			RTtoLT.gameObject.SetActive (false);
+			LBtoLT.gameObject.SetActive (true);
+			LBtoRB.gameObject.SetActive (false);
+			LBtoRT.gameObject.SetActive (false);
+			LTtoRB.gameObject.SetActive (false);
+			RBtoLT.gameObject.SetActive (false);
+
+			ori.gameObject.SetActive (false);
+
+			boolLB = false;
+			boolLT = false;
+		}
+
+		if(boolLB == true && boolRB == true)
+		{
+			RTtoRB.gameObject.SetActive (false);
+			RTtoLB.gameObject.SetActive (false);
+			RTtoLT.gameObject.SetActive (false);
+			LBtoLT.gameObject.SetActive (false);
+			LBtoRB.gameObject.SetActive (true);
+			LBtoRT.gameObject.SetActive (false);
+			LTtoRB.gameObject.SetActive (false);
+			RBtoLT.gameObject.SetActive (false);
+
+			ori.gameObject.SetActive (false);
+
+			boolLB = false;
+			boolRB = false;
+		}
+
+		if(boolLB == true && boolRT == true)
+		{
+			RTtoRB.gameObject.SetActive (false);
+			RTtoLB.gameObject.SetActive (false);
+			RTtoLT.gameObject.SetActive (false);
+			LBtoLT.gameObject.SetActive (false);
+			LBtoRB.gameObject.SetActive (false);
+			LBtoRT.gameObject.SetActive (true);
+			LTtoRB.gameObject.SetActive (false);
+			RBtoLT.gameObject.SetActive (false);
+
+			ori.gameObject.SetActive (false);
+
+			boolLB = false;
+			boolRT = false;
+		}
+
+		if(boolLT == true && boolRB == true)
+		{
+			RTtoRB.gameObject.SetActive (false);
+			RTtoLB.gameObject.SetActive (false);
+			RTtoLT.gameObject.SetActive (false);
+			LBtoLT.gameObject.SetActive (false);
+			LBtoRB.gameObject.SetActive (false);
+			LBtoRT.gameObject.SetActive (false);
+			LTtoRB.gameObject.SetActive (true);
+			RBtoLT.gameObject.SetActive (false);
+
+			ori.gameObject.SetActive (false);
+
+			boolLT = false;
+			boolRB = false;
+		}
+
+	}
+
+//	void reset()
+//	{
+//		bool boolLT = false;
+//		bool boolRT = false;
+//		bool boolRB = false;
+//		bool boolLB = false;
+//	}
+
+	public void clickLT()
+	{
+		boolLT = true;
+	}
+
+	public void clickRT()
+	{
+		boolRT = true;
+	}
+
+	public void clickRB()
+	{
+		boolRB = true;
+	}
+
+	public void clickLB()
+	{
+		boolLB = true;
+	}
+
+}
---
+++ Assets/Scripts/PrintePage.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrintePage : MonoBehaviour {
+
+	public GameObject Yb;
+	public GameObject Yp;
+	public GameObject Rb;
+	public GameObject Rp;
+	public GameObject Bb;
+	public GameObject Bp;
+	public GameObject Db;
+	public GameObject Dp;
+
+	public void YbClick()
+	{
+		//Debug.Log ("Click");
+		Yp.gameObject.SetActive (true);
+		Bp.gameObject.SetActive (false);
+		Rp.gameObject.SetActive (false);
+		Dp.gameObject.SetActive (false);
+	}
+
+	public void BbClick()
+	{
+		//Debug.Log ("Click");
+		Yp.gameObject.SetActive (false);
+		Bp.gameObject.SetActive (true);
+		Rp.gameObject.SetActive (false);
+		Dp.gameObject.SetActive (false);
+	}
+
+	public void RbClick()
+	{
+		//Debug.Log ("Click");
+		Yp.gameObject.SetActive (false);
+		Bp.gameObject.SetActive (false);
+		Rp.gameObject.SetActive (true);
+		Dp.gameObject.SetActive (false);
+	}
+
+	public void DbClick()
+	{
+		//Debug.Log ("Click");
+		Yp.gameObject.SetActive (false);
+		Bp.gameObject.SetActive (false);
+		Rp.gameObject.SetActive (false);
+		Dp.gameObject.SetActive (true);
+	}
+
+}
