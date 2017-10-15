06447ad30936b9dbc30209d6a1ef2aa54b58ff2c
445710992an <31056280+445710992an@users.noreply.github.com>
2017-10-15 19:19:18 +1100

    change

---
+++ Assets/panelScripts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class panelScripts : MonoBehaviour {
+
+	public GameObject Panel;
+	int counter;
+
+
+	public void showhidePanel()
+	{
+		Panel.gameObject.SetActive (false);
+//		counter++;
+		if (counter % 2 == 1) {
+			Panel.gameObject.SetActive (false);
+		} else {
+			Panel.gameObject.SetActive (true);
+		}
+		counter++;
+	}
+}
